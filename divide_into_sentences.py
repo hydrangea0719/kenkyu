62a2c36853687905ffa5f7093117894ac2c82ea4
Mizuki Tada <tada@ss.cs.osakafu-u.ac.jp>
2020-12-15 21:49:28 +0900

    データ前処理の更新

--- divide_into_sentences.py
+++ divide_into_sentences.py
@@ -4,12 +4,14 @@
 
 
 import csv
+import re
+import unicodedata
 
 # dataset の中にあるcsvファイルを読み込みたい（願望）
 # 自作関数のパッケージ化と同じように，ディレクトリに mai2008_01.csv と __init__.py を入れてみた
 # from dataset import mai2008_01 as maidata
 # できんが？？？？？？？？？？？？わからん
-# dataset の中に入れるのはスクリプト
+# dataset の中に入れるのはスクリプト！！！
 
 csv_file = open("mai2008_01.csv", 'r')
 
@@ -24,45 +26,68 @@ del a_list[0]
 # csv_file.close()
 
 
+# 最初に文字の置換をする
+# 次に句点で分ける
+
 a_text = ""
 for a in a_list:
     # 句点で区切る
     l_sentences = a.split('。')
-    # l_sentences = [line.strip("[SEP]") for line in l_sentences if line.startswith("[SEP]")]
     a_text += '。\n'.join(l_sentences)
-    # a_text += "\n"
 
-# .join() ha list -> str ni henkan
+# 文字表記の統一
+a_text = unicodedata.normalize('NFKC', a_text)
 
+
+# .replace().replace() は操作を複数回呼んでいるだけ
+# 1：[SEP]タグで改行，2：意味のない文を削除
 a_text = a_text.replace('[SEP]', '\n').replace('【現在著作権交渉中の為、本文は表示できません】', '')
 
 
-# .replace().replace() 複数回呼んでいるだけ
 
+# .join()は list を str に変換する
+
+# table を使って置換することもできる
 table = str.maketrans({
     '\u3000': '',
     ' ': '',
     '\t': '',
     '◇': '',
-    '…': '',
+    '.': '',
     '■': '',
-    '▲': '\n',  # kore ha kuten no kawari ni tukawarete-iru mitai
-    '▽': '\n'  # kore ha kajou-gaki ?
+    '*': '',
+    '▲': '。',  # これは句点の代わりに使われているみたい
+    '▽': '、'  # これは箇条書きの代わりに使われているみたい
 })
-
 a_text = a_text.translate(table)
-a_text = a_text.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
 
+# # これは全角半角の変換．半角に統一する
+# a_text = a_text.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
+# # FF01 = 全角の ！，FF5E = 全角の 〜
+# # 21 = 半角の !，7E = 半角の ~
+# # https://ja.wikipedia.org/wiki/Unicode%E4%B8%80%E8%A6%A7_0000-0FFF
+
+
+# a_text = re.sub(r'\s+', '', a_text)    # これは複数の連続する空白を1つに置換する
+
+
+#
+a_text = re.sub(r'\(.+?\)', '', a_text)
+a_text = re.sub(r'【.+?】', '', a_text)
+
+
+# 一行に着目して strip した結果が空文でないなら，改行記号で split して list 型にする
+# それを改行記号を挟みながら str 型にする
 a_text = '\n'.join([line for line in a_text.split('\n') if not line.strip() == ''])
 
-#for line in a_text:
-#    if line == "\n":
-#        line.replace("\n", "")
 
+
+
+
+#
 file_output = open("mai2008_01.txt", 'w')
 file_output.writelines(a_text)
 file_output.close()
-
 
 # s = "aaaab"
 # s = s.replace("aa", "a")
@@ -72,11 +97,41 @@ file_output.close()
 
 # csvファイルから本文のみを抽出し，句点区切りで１文ずつに分けることができましたやったね！！！
 
+
 # （２２面に関連記事）[SEP]　判決を受け、下山判事は法曹資格を失った。
-# [SEP] tagu demo kugiru beki?
+# [SEP] タグも一文とカウントして区切るべき？
+# -> とりあえず区切ることにした
+
 # 什〓(じゅうほう)市の化学工場の倒壊現場
-# moji-bake ?
+# 文字化け？どう対応するか
 
-# strip
+# 【高瀬浩平、写真・森園道子】<1日1句医者いらず>
+# ここらへんなんとかしたい，「」は消せないけれど，それ以外の括弧は消せそう
+# 【 で検索をかけると4500件ヒットする
+# -> とりあえず消した
+
+# ◆電気料金の試算結果◆
+# 電力会社1〜3月5月最大差額
+# 北海道67966200〜6400▼596
+# 東北68856300〜6500▼585
+# 東京72066000〜6400▼1206
+# 中部71936500〜6800▼693
+# 北陸68266400〜6600▼426
+# 関西68716400〜6600▼471
+# 中国74366900〜7100▼536
+# 四国70016700〜6800▼301
+# 九州66286300〜6500▼328
+# 沖縄82427400〜7800▼842
+# ※標準家庭の1カ月当たりの料金、単位・円。
+# 1〜3月は確定値。
+# なんやこいつ？？？？？？？？？？
+
+# でもワースト1
+# ？？？？？
+# [SEP]　◇でもワースト１[SEP]　
+# これはブチギレ案件
+
+
+
+
 # https://note.nkmk.me/python-str-remove-strip/
-
